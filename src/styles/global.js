30c4b890d352543604c4fef6c57293ba27c11ef7
Bruno Alexandre <brunoalencaralexandre@gmail.com>
2019-08-15 00:41:34 -0300

    All application

---
+++ src/styles/global.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+import 'font-awesome/css/font-awesome.css';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  body {
+    font-family: sans-serif;
+    background: #9B65E6 !important;
+    text-rendering: optimizeLegibility !important;
+    -webkit-font-smoothing: antialiased !important;
+  }
+`;
+
+export default GlobalStyle;
